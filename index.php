3b5cff46afff3ec1359c3e3e70f4083b512979ea
VictorVolkoff <viktor.volk0vv5@gmail.com>
2023-03-02 12:11:52 +0300

    create_files

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WebApplication</title>
-</head>
-<body>
-    
-</body>
-</html>
+$page_title = "Main Web";
+$title = "1";
+
+require('templates/layout.php');
+
